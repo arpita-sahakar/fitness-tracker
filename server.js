8d0719073a47eb1f3cdc1a748534f4b7bd9a6f86
Arpita Saha <arpita.sahaa@gmail.com>
2021-01-06 18:27:12 -0500

    Added crud routes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 
-
+// -----------------------------------------------------------------------------
 
 app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
@@ -33,7 +33,81 @@ app.get("/api/workouts", (req, res) => {
       });
   });
 
+  app.get("/api/workouts/:id", (req, res) => {
+    db.Workout.findOne({_id:req.params.id})
+    //   .populate("exercise")
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 
+  app.get("/api/workoutsById/:id", (req, res) => {
+    db.Workout.findById({_id:req.params.id})
+    //   .populate("exercise")
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+// -------------------------------------------------------------------------------------
+  app.post("/api/workouts",(req,res) =>{
+    db.Workout.create(req.body)
+    .then(data =>{
+      res.json(data);
+    })
+    .catch(err =>{
+      res.json(err);
+    });
+  });
+
+//----------------------------------------------------------------------------------------
+  app.delete("/api/delWorkOut/",(req,res)=>{
+    const itemToBeDeleted = req.body.day;
+    db.Workout.deleteOne({day :itemToBeDeleted })
+    .then(data=>{
+      res.json(data);
+    })
+    .catch(err =>{
+      res.json(err);
+    });
+  })
+
+
+  app.delete("/api/workouts",(req,res)=>{
+    db.Workout.deleteMany({day:{$gte:req.body.day}})
+    .then(data=>{
+      res.json(data);
+    })
+    .catch(err =>{
+      res.json(err);
+    });
+  })
+
+
+  app.delete("/api/workoutsById/:id",(req,res)=>{
+    db.Workout.findByIdAndDelete(req.params.id)
+    .then(data=>{
+      res.json(data);
+    })
+    .catch(err =>{
+      res.json(err);
+    });
+  })
+
+
+//---------------------------------------------------------------------------------------
+
+app.put("api/workouts/:id",(req,res)=>{
+  db.Workout.findOneAndUpdate({_id:req.params.id},req.body)
+  .then(data=>{
+    res.json(data);
+  })
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
